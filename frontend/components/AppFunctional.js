04706462f7eb24251af294b9cbb02592d51bbaa2
Robert Jank <robert.jank@yahoo.com>
2024-08-19 16:33:34 -0500

    add grid movment to the buttons and a rest to reset all states back to initial value.

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 // Suggested initial states
 const initialMessage = "";
@@ -10,6 +11,8 @@ const initialGrid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 export default function AppFunctional(props) {
   const [grid, setGrid] = useState(initialGrid);
   const [steps, setSteps] = useState(initialSteps);
+  const [formData, setFormData] = useState(initialEmail);
+  const [message, setMessage] = useState(initialMessage);
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
@@ -17,29 +20,37 @@ export default function AppFunctional(props) {
     return array.indexOf(4);
   }
 
-  function getXY() {}
-
-  function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+  function getXY(index) {
+    if (index < 3) return `(${index + 1}, 1)`;
+    if (index > 5) return `(${index - 5}, 3)`;
+    else return `(${index - 2}, 2)`;
   }
+
+  function trackXY() {}
 
   function reset(evt) {
     evt.preventDefault();
     setGrid(initialGrid);
     setSteps(initialSteps);
+    setFormData(initialEmail);
+    setMessage(initialMessage);
     // Use this helper to reset all states to their initial values.
   }
 
   function getNextIndex(direction, idx) {
     let currentIdx = idx;
+    setMessage(initialMessage);
     if (direction === "up" && idx > 2) currentIdx -= 3;
+    else if (direction === "up" && idx < 3) setMessage("You can't go up");
+
     if (direction === "down" && idx < 6) currentIdx += 3;
+    else if (direction === "down" && idx > 5) setMessage("You can't go down");
     if (direction === "left" && idx !== 0 && idx !== 3 && idx !== 6)
       currentIdx -= 1;
+    else if (direction === "left" && idx < 3) setMessage("You can't go left");
     if (direction === "right" && idx !== 2 && idx !== 5 && idx !== 8)
       currentIdx += 1;
+    else if (direction === "right" && idx < 3) setMessage("You can't go right");
 
     return currentIdx;
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
@@ -63,22 +74,39 @@ export default function AppFunctional(props) {
       newGrid.splice(nextIdx, 0, 4);
       setGrid(newGrid);
       setSteps(steps + 1);
-      console.log(steps);
     }
   }
 
   function onChange(evt) {
+    setFormData(evt.target.value);
+    console.log(evt);
     // You will need this to update the value of the input.
   }
 
-  function onSubmit(evt) {
+  const onSubmit = async (evt) => {
+    evt.preventDefault();
+    if (!formData) setMessage("Ouch: email is required");
+    else if (!formData.includes(".com"))
+      setMessage("Ouch: email must be a valid email");
+    setFormData(initialEmail);
+    try {
+      const { data } = await axios.post("http://localhost:9000/api/result", {
+        x: 1,
+        y: 2,
+        steps: 4,
+        email: "name@email.com",
+      });
+      setMessage(data.message);
+    } catch (err) {
+      console.error(err);
+    }
     // Use a POST request to send a payload to the server.
-  }
+  };
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
+        <h3 id="coordinates">Coordinates {getXY(getIndex(grid))}</h3>
         <h3 id="steps">You moved {steps} times</h3>
       </div>
       <div id="grid">
@@ -89,7 +117,7 @@ export default function AppFunctional(props) {
         ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
         <button onClick={move} id="left">
@@ -108,8 +136,14 @@ export default function AppFunctional(props) {
           reset
         </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={formData}
+          id="email"
+          type="email"
+          placeholder="type email"
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
